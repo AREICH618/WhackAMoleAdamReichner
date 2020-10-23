bd64a40d11f28892bcf14bdf47bc64ef87186b8e
Adam <adam.reichner.049@k12.friscoisd.org>
2020-10-23 10:49:04 -0500

    Initial Commit

---
+++ WhackAMoleAdamReichner/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WhackAMoleAdamReichner
+//
+//  Created by Reichner, Adam R on 10/23/20.
+//  Copyright © 2020 Reichner, Adam R. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
